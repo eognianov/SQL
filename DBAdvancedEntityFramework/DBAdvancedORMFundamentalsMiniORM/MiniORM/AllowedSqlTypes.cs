c45a3189dc99ede670899fa88721932d7299f1aa
eognianov <emilian_89@abv.bg>
2018-07-03 04:04:49 +0300

    MiniORM

---
+++ DBAdvancedEntityFramework/DBAdvancedORMFundamentalsMiniORM/MiniORM/AllowedSqlTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniORM
+{
+    internal static class AllowedSqlTypes
+    {
+        internal static Type[] SqlTypes = new Type[]
+        {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(bool),
+            typeof(DateTime)
+
+    };
+    }
+}
